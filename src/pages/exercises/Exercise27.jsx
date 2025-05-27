02f966e23d0fcd4e60a5206cbdf7d366bdaeb958
Maxence Hammen <maxence.hammen@gmail.com>
2025-05-27 10:01:56 +0200

    Change used API

--- src/pages/exercises/Exercise27.jsx
+++ src/pages/exercises/Exercise27.jsx
@@ -4,8 +4,8 @@ export default function Exercise27() {
       <p>
         Écrire une application qui à chaque rechargement de page écrit une
         nouvelle citation. Vous utilisez l&apos;API{" "}
-        <a href="https://api.quotable.io/random">
-          https://api.quotable.io/random
+        <a href="https://api.breakingbadquotes.xyz/v1/quotes">
+          https://api.breakingbadquotes.xyz/v1/quotes
         </a>
       </p>
     </>
